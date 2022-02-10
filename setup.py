1ae69481fdf38778fce07c64e91cec9bd62fb1a9
Stefan Postolache <stefan.freya@gmail.com>
2022-02-10 18:49:25 +0000

    Updated class diagram

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
 
+with open("Readme.md") as fh:
+    long_description = fh.read()
+
 setup(
     name='sp_algopyformance',
     packages=find_packages(include=['sp_algopyformancelib']),
     version='0.1.0',
     description="A package to aid students of ISCTE in testing their algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stefanpostolache/algopyformance",
     author="Stefan Postolache",
+    author_email="stefanogiovanochestii@gmail.com",
     license='MIT',
     install_requires=['Pillow', 'cycler', 'matplotlib', 'seaborn', 'pandas',
                       'numpy', 'typing', 'typing-extensions', 'fonttools',
@@ -13,5 +20,10 @@ setup(
                       'python-dateutil', 'pytz', 'scipy', 'six'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    test_suite='tests'
+    test_suite='tests',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
